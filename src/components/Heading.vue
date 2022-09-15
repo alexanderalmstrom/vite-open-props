865b2732c32ccc8b2a333ecbc36338cd819fd827
Alexander Almström <alexander.almstrom@madepeople.se>
2022-09-16 01:40:15 +0200

    Headings and dynamic import support

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -16,3 +16,16 @@ const props = defineProps({
   },
 })
 </script>
+
+<style>
+.heading {
+  color: var(--heading-color);
+  font-family: var(--heading-font-family);
+  font-size: var(--heading-font-size);
+  font-weight: var(--heading-font-weight);
+  line-height: var(--heading-line-height);
+  letter-spacing: var(--heading-letter-spacing);
+  text-transform: var(--heading-text-transform);
+  margin-bottom: var(--heading-margin-bottom);
+}
+</style>
