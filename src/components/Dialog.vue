0fcc22f7d99b20bb14983b133d802c405fc27cfd
Alexander Almström <alexander.almstrom@madepeople.se>
2022-09-19 16:41:37 +0200

    Messing around with ref and dialog with composition api

---
+++ src/components/Dialog.vue
@@ -0,0 +1,17 @@
+<template>
+  <dialog ref="dialog">
+    <slot />
+  </dialog>
+</template>
+
+<script setup>
+import { ref, defineExpose } from 'vue'
+
+const dialog = ref(null)
+
+defineExpose({
+  dialog,
+})
+</script>
+
+<style></style>
